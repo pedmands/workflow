583b27ae2f72cda3271fc60304eead428f1a3300
Preston Edmands <pedmands@gmail.com>
2015-08-13 17:45:24 -0400

    Concatenated JavaScript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 var gulp = require('gulp'),
 		gutil = require('gulp-util'),
-		coffee = require('gulp-coffee');
+		coffee = require('gulp-coffee'),
+		concat = require('gulp-concat');
+		
+var jsSources = [
+	'components/scripts/rclick.js',
+	'components/scripts/pixgrid.js',
+	'components/scripts/tagline.js',
+	'components/scripts/template.js'
+]		
 		
 gulp.task('coffee', function() {
 	gulp.src('components/coffee/tagline.coffee')
@@ -9,3 +17,8 @@ gulp.task('coffee', function() {
 		.pipe(gulp.dest('components/scripts'))
 });
 
+gulp.task('js', function(){
+	gulp.src(jsSources)
+		.pipe(concat('script.js'))
+		.pipe(gulp.dest('builds/development/js'))
+});
