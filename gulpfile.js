8850d40a1f3a27f16bfd90b061c0a40dc2046b95
Preston Edmands <pedmands@gmail.com>
2015-08-13 18:08:48 -0400

    Added compass

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require('gulp'),
 		gutil = require('gulp-util'),
 		coffee = require('gulp-coffee'),
 		browserify = require('gulp-browserify'),
-		concat = require('gulp-concat');
+		compass = require('gulp-compass'),
+		concat = require('gulp-concat'),
+		cleanDest = require('gulp-clean-dest');
 		
 var jsSources = [
 	'components/scripts/rclick.js',
@@ -10,7 +12,11 @@ var jsSources = [
 	'components/scripts/tagline.js',
 	'components/scripts/template.js'
 ]		
-		
+
+var sassSources = [
+	'components/sass/style.scss'
+]
+
 gulp.task('coffee', function() {
 	gulp.src('components/coffee/tagline.coffee')
 		.pipe(coffee({bare:true})
@@ -23,4 +29,17 @@ gulp.task('js', function(){
 		.pipe(concat('script.js'))
 		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'))
+});
+
+gulp.task('compass', function(){
+	gulp.src(sassSources)
+		.pipe(compass({
+			sass: 'components/sass',
+			image: 'builds/development/images',
+			style: 'expanded',
+			comments: true
+		})
+			.on('error', gutil.log))
+		.pipe(gulp.dest('builds/development/css'))
+		.pipe(cleanDest('css'))
 });
